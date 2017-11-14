5533d2e918910dce70ca3b3426893cb5b2fd583d
Anthony Graddy <anthony@locationq.com>
2017-11-14 23:38:06 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,8 +1,25 @@
-process.chdir('test');
+var path = require('path');
+process.chdir(path.dirname(__filename));
 var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+start();
+
+function start() {
+	var req = {};
+	var res = {};
+	var lug = {};
+	req.url = '/one/two/three?four=4&five=5';
+
+
+	mod(req, res, lug, function() {
+		tap.assert.deepEqual(lug.get, {"four": "4", "five": "5"}, 'Should set get.');
+		tap.assert.deepEqual(lug.slugs, ['one', 'two', 'three'], 'Should set slugs.');
+	});
+}
+
+function end() {
+}
 
 
